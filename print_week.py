a30d4f4d1466d51e9f7e3bbecda2bff8cecbd628
harrystudypython <52101807+harrystudypython@users.noreply.github.com>
2021-01-12 22:23:21 +0800

    Create print_week.py

---
+++ print_week.py
@@ -0,0 +1,6 @@
+import datetime
+datetime.datetime.now().isocalendar()
+from datetime import datetime 
+apply_time = "2023-2-1"
+week=datetime.strptime('20210112','%Y%m%d').strftime('%W')
+print week
